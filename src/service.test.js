031502207b877717626b76100d05cbd6bf2ea265
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-11-29 20:40:02 -0300

    Test stub

--- src/service.test.js
+++ src/service.test.js
@@ -1,8 +1,17 @@
 const Service = require("./service");
+const sinon = require("sinon");
 const DEFAULT_URL_1 = "https://swapi.dev/api/planets/3/";
 const DEFAULT_URL_2 = "https://swapi.dev/api/planets/4/";
+const mocks = {
+  yavin: require("./mocks/yavin_IV.json"),
+  hoth: require("./mocks/hoth.json"),
+};
 (async () => {
   const service = new Service();
-  const result = await service.makeRequest(DEFAULT_URL_2);
-  console.log(JSON.stringify(result));
+  const stubs = sinon.stub(service, service.makeRequest.name);
+  stubs.withArgs(DEFAULT_URL_1).resolves(mocks.yavin);
+  stubs.withArgs(DEFAULT_URL_2).resolves(mocks.hoth);
+
+  const result = await service.makeRequest(DEFAULT_URL_1);
+  console.log(result);
 })();
